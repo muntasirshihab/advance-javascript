703493c24188df912a21da9650ec362cecde980c
MunTasir ShihaB <muntasir.shihab@gmail.com>
2021-09-27 22:43:21 +0600

    abcd

---
+++ scope.js
@@ -0,0 +1,16 @@
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result>9){
+        var mood = "Happy";
+        console.log(mood);
+    }
+    console.log(mood);
+    return result;
+}
+
+const output = sum (3,7);
+
+console.log(day);
+let day = "friday";
